d2c28e716ab2d37f240f2113b712c72ced28dde8
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2018-03-09 17:01:44 +0100

    0.0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='litkit',
     packages=['litkit'],
-    version='0.0.8',
+    version='0.0.9',
     description='data analysis convenience package',
     author='Philipp Woerdehoff',
     author_email='ph.woerdehoff@gmail.com',
